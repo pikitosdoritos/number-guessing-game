de9cff873d0e416b863ea55adf0eaf0cef64c091
pikitosdoritos <fastovetsn@gmail.com>
2026-03-13 18:06:49 +0200

    Implement record saving and retrieval, enhance level selection, and improve user input validation

--- main.py
+++ main.py
@@ -2,18 +2,58 @@ import random
 import json
 import os
 
+FILE="record.json"
+
 guess_number = random.randint(1, 100)
 
 low = 1
 high = 100
 
 attempts = 0
-max_attempts = 0
 
 running = True
 
-def select_level():
+def save_record(name, attempts, level):
+    if not os.path.exists(FILE):
+        data = []
+    else:
+        with open(FILE, "r") as f:
+            data = json.load(f)
+            
+    data.append({
+        "name": name,
+        "attempts": attempts,
+        "level": level,
+    })
+    
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+def get_record():
+    with open(FILE, "r") as f:
+        data = json.load(f)
+        
+    data = sorted(data, key=lambda x: x["attempts"], reverse=True)
+    
+    return data
+
+def print_record():
+    records = get_record()
+    
+    print("\nTABLE OF RECORDS")
+    print("-----------------")
+
+    for i, r in enumerate(records[:10], start=1):
+        print(f"{i}. {r['name']} - {r['score']}")
+
+def select_level_and_name():
     while True:
+        name = input("Enter your name: ")
+        
+        if not name.isalpha():
+            print("Please enter a valid name.") 
+            continue
+        
         select_level = input("Select a level: 'easy', 'medium', 'hard': ")
             
         if select_level.lower() == "easy":
@@ -30,9 +70,9 @@ def select_level():
             
     print(f"You have {max_attempts} attempts to guess the number.")
             
-    return max_attempts
-    
-max_attempts = select_level()
+    return name, select_level, max_attempts
+
+name, selected_level, max_attempts = select_level_and_name()
   
 while running:   
     user_input = (input("Guess a number between (1 and 100) and 'hint' to take a hint': "))
@@ -50,6 +90,7 @@ while running:
     
     if guess == guess_number:
         print(f"Congratulations! You guessed the number: {guess_number} in {attempts} attempts.")
+        save_record(name, attempts, selected_level)
         running = False
     
     elif attempts == max_attempts:
